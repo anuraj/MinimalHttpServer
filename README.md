f9344bbc4d9df6fd65e707914032de17b65912e3
Anuraj <aparameswaran@socxo.com>
2024-05-22 16:37:35 +0530

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# SimpleHttpServer
+# Simple Http Server
+
+This is a simple http server built using ASP.NET Core which helps to serve static files.
---
+++ Src/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDirectoryBrowser();
+
+var app = builder.Build();
+
+app.UseDirectoryBrowser(new DirectoryBrowserOptions
+{
+    FileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory())
+});
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.Run();
